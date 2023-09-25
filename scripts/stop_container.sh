5300f7c63feb6ea63ce9d947fc5e21abba516510
VENKATA SURESH <118714153+venkatasureshborra@users.noreply.github.com>
2023-09-25 16:58:25 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,5 +2,5 @@
 set -e
 
 # Stop the running container (if any)
-containerid=`sudo docker ps | awk -F " " '{print $1}'`
+containerid=`sudo docker ps | awk 'NR==2 {print $1}'`
 sudo docker rm -f $containerid
